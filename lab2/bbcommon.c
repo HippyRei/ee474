06a3ed5d427b8dfd40866adff8c792243bdf4467
Guest <guest-xqodcc@dhcp141-188.ee.washington.edu>
2016-10-13 18:48:11 -0700

    Added GPIO constants, added initializeLCD function and started it, added isetGPIO function to write integers to files, implemented setPins function to set different configurations of GPIOs

--- lab2/bbcommon.c
+++ lab2/bbcommon.c
@@ -1,6 +1,12 @@
 // A collection of beaglebone operations used by multiple programs
 #include "bbcommon.h"
 
+int GPIO_NUMS[] = {49, 117, 26, 44, 68, 67, 47, 45, 69, 66, 115};
+
+char * GPIO_VALS[] = {RS_VAL, RW_VAL, DB7_VAL, DB6_VAL, DB5_VAL, DB4_VAL, DB3_VAL, DB2_VAL, DB1_VAL, DB0_VAL, E_VAL};
+
+char * GPIO_DIRS[] = {RS_DIR, RW_DIR, DB7_DIR, DB6_DIR, DB5_DIR, DB4_DIR, DB3_DIR, DB2_DIR, DB1_DIR, DB0_DIR, E_DIR};
+
 void activateGPIO(int gnum) {
   // Attempt to open the file; loop until file is found
   FILE *f = NULL;
@@ -22,4 +28,29 @@ void setGPIO(char * path, char * flag) {
   
   fprintf(f, "%s", flag);
   fclose(f);
+}
+
+void isetGPIO(char * path, int flag) {
+  // Attempt to open the file; loop until file is found
+  FILE *f = NULL;
+  while (f == NULL) {
+    f =  fopen(path, "w");
+  }
+  
+  fprintf(f, "%d", flag);
+  fclose(f);
+}
+
+void initializeLCD() {
+  for (int i = 0; i < 11; i++) {
+    activateGPIO(GPIO_NUMS[i]);
+    setGPIO(GPIO_DIRS[i], "out");
+  }
+
+}
+
+void setPins(int * config, int len) {
+  for (int i = 0; i < len; i++) {
+    isetGPIO(GPIO_VALS[i], config[i]);
+  }
 }
--- lab2/bbcommon.h
+++ lab2/bbcommon.h
@@ -2,10 +2,33 @@
 #define BBCOMMON_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 #define GPIO20_VAL "../../sys/class/gpio/gpio20/value"
+#define DB0_VAL "../../sys/class/gpio/gpio66/value"
+#define DB1_VAL "../../sys/class/gpio/gpio69/value"
+#define DB2_VAL "../../sys/class/gpio/gpio45/value"
+#define DB3_VAL "../../sys/class/gpio/gpio47/value"
+#define DB4_VAL "../../sys/class/gpio/gpio67/value"
+#define DB5_VAL "../../sys/class/gpio/gpio68/value"
+#define DB6_VAL "../../sys/class/gpio/gpio44/value"
+#define DB7_VAL "../../sys/class/gpio/gpio26/value"
+#define RS_VAL "../../sys/class/gpio/gpio49/value"
+#define RW_VAL "../../sys/class/gpio/gpio117/value"
+#define E_VAL "../../sys/class/gpio/gpio115/value"
 
 #define GPIO20_DIR "../../sys/class/gpio/gpio20/direction"
+#define DB0_DIR "../../sys/class/gpio/gpio66/direction"
+#define DB1_DIR "../../sys/class/gpio/gpio69/direction"
+#define DB2_DIR "../../sys/class/gpio/gpio45/direction"
+#define DB3_DIR "../../sys/class/gpio/gpio47/direction"
+#define DB4_DIR "../../sys/class/gpio/gpio67/direction"
+#define DB5_DIR "../../sys/class/gpio/gpio68/direction"
+#define DB6_DIR "../../sys/class/gpio/gpio44/direction"
+#define DB7_DIR "../../sys/class/gpio/gpio26/direction"
+#define RS_DIR "../../sys/class/gpio/gpio49/direction"
+#define RW_DIR "../../sys/class/gpio/gpio117/direction"
+#define E_DIR "../../sys/class/gpio/gpio115/direction"
 
 #define GPIO_EXPORT_PATH "/sys/class/gpio/export"
 
@@ -14,5 +37,9 @@ void activateGPIO(int gnum);
 
 // Writes flag to the file associated with path
 void setGPIO(char * path, char * flag);
+
+void isetGPIO(char * path, int flag);
+
+void initializeLCD();
 
 #endif
