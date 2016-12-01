f54dcb44062a93d683644c10fb112c345c1c9a52
Guest <guest-yibhgr@dhcp141-192.ee.washington.edu>
2016-11-30 19:07:21 -0800

    got send on bluetooth working

--- lab5/bt_listener.c
+++ lab5/bt_listener.c
@@ -5,10 +5,79 @@ struct termios ti;
 
 
 int main() {
+  int f, len;
+  unsigned char buffer[100];
+
+  printf("entered loop\n");
+  
   enable_UART1();
 
-  cfsetospeed(&ti, 
+  printf("entered loop\n");
 
+  usleep(1000);
+  //while (!!access(UART1, X_OK));
+  f = open(UART1, O_RDWR | O_NOCTTY | O_SYNC);
+
+  if (f < 0) {
+    printf("file failed to open\n");
+  }
+
+  memset (&ti, 0, sizeof ti);
+
+  if (tcgetattr(f, &ti) != 0) {
+    printf("tcgetattr failed\n");
+  }
+
+  cfsetospeed(&ti, BAUDRATE);
+  cfsetispeed(&ti, BAUDRATE);
+
+  // control flag
+  // ti.c_cflag = 0;
+  ti.c_cflag |= (CREAD | CS8);
+  ti.c_cflag |= CRTSCTS;
+  ti.c_cflag &= ~(PARENB | PARODD);
+  ti.c_cflag &= ~CSTOPB;
+
+  // input flag
+  ti.c_iflag = 0;
+  //ti.c_iflag &= ~(IXON | IXOFF | IXANY);
+  ti.c_iflag |= (IGNPAR | ICRNL);
+
+  // local flag
+  ti.c_lflag = 0;
+  ti.c_lflag &= ~ICANON;
+
+  // output flag
+  ti.c_oflag = 0;
+
+  ti.c_cc[VMIN] = 0;
+  ti.c_cc[VTIME] = 5;
+
+  tcflush(f, TCIFLUSH);
+
+  if (tcsetattr(f, TCSANOW, &ti) != 0) {
+    printf("tcsetattr failed\n");
+  }
+  printf("finished initializing %d\n",f);
+
+  len = write(f, "hello\n", 6);
+  printf("value of len: %d\n", len);
+  
+
+  while (1) {
+    printf("start of while loop here\n");
+    len = read(f, &buffer, 5);
+    buffer[len] = 0;
+    printf("value of len: %d\n", len);
+  
+    if (len != 0) {
+      printf("%s\n", buffer);
+    } else {
+      //printf("nothing read\n");
+    }
+  }
+  close(f);
+  
 }
 
 void enable_UART1() {
--- lab5/bt_listener.h
+++ lab5/bt_listener.h
@@ -1,6 +1,8 @@
 #ifndef BT_LISTENER_H
 #define BT_LISTENER_H
 
+#define _BSD_SOURCE
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -9,12 +11,13 @@
 #include <sys/types.h>
 #include <time.h>
 #include <termios.h>
+#include <fcntl.h>
 
 #define DEVPATH "/sys/devices/bone_capemgr.9/slots"
 
 #define BAUDRATE B115200
 
-#define UART1 "/dev/tty01"
+#define UART1 "/dev/ttyO1"
 
 void enable_UART1();
 
