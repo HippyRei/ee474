53e3c3761e208131507acd168339ac3526c2c8fe
Reilly Mulligan <Reilly Mulligan>
2016-12-06 19:48:39 -0800

    Fixed slider, added landscape layout, and working on bluetooth

--- lab5/lab5_app/TankDrive/app/src/main/java/com/example/group10/tankdrive/MainActivity.java
+++ lab5/lab5_app/TankDrive/app/src/main/java/com/example/group10/tankdrive/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.group10.tankdrive;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+    protected int requestCode = 0;
+    protected BluetoothAdapter mBluetoothAdapter;
+    private static final UUID MY_UUID = new UUID(32, 32);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            return;
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, requestCode);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        boolean isPaired = false;
+
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                if (device.getAddress().equals(getString(R.string.tank_address))) {
+                    isPaired = true;
+                }
+            }
+        }
+
+        
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        this.requestCode = requestCode;
     }
 }
